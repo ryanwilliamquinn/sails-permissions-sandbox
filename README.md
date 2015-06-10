948afe0936b61ee30808e321a2fb94c8475d8a99
ryan quinn <ryanwilliamquinn@gmail.com>
2015-06-10 09:16:07 -0400

    set up sails permissions sandbox

---
+++ README.md
@@ -0,0 +1,3 @@
+# messageperms
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/User.js
@@ -0,0 +1,20 @@
+// api/models/User.js
+
+var _ = require('lodash');
+var _super = require('sails-permissions/api/models/User');
+
+_.merge(exports, _super);
+_.merge(exports, {
+
+  // Extend with custom logic here by adding additional fields, methods, etc.
+
+  /**
+   * For example:
+   *
+   * foo: function (bar) {
+   *   bar.x = 1;
+   *   bar.y = 2;
+   *   return _super.foo(bar);
+   * }
+   */
+});
