c0b0160fbb2cd20e4137f7f8f610455f085c0d17
ryan quinn <ryanwilliamquinn@gmail.com>
2015-06-10 09:34:17 -0400

    fix some more stuff

---
+++ api/controllers/MessageController.js
@@ -0,0 +1,11 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
--- api/services/PermissionService.js
+++ api/services/PermissionService.js
@@ -3,23 +3,77 @@
 var _ = require('lodash');
 var _super = require('sails-permissions/api/services/PermissionService');
 
-function PermissionService () { }
+function PermissionService() {}
 
 PermissionService.prototype = Object.create(_super);
-_.extend(PermissionService.prototype, {
 
-  // Extend with custom logic here by adding additional fields and methods,
-  // and/or overriding methods in the superclass.
+PermissionService.prototype.addRole = function(userName, roleName) {
+  User.findOne({
+      username: userName
+    })
+    .then(function(user) {
+      return Role.findOne({
+          name: roleName
+        })
+        .populate('users')
+        .then(function(role) {
+            console.log('hrm?', role);
+          if (role) {
+            role.users.push(user);
+            console.log(role.users);
+            return Role.update({
+              name: roleName
+            }, {
+              users: role.users
+            })
+          } else {
+            return Role.create({
+              name: roleName,
+              users: [user.id]
+            });
 
-  /**
-   * For example:
-   *
-   * foo: function (bar) {
-   *   bar.x = 1;
-   *   bar.y = 2;
-   *   return _super.foo.call(this, bar);
-   * }
-   */
-});
+          }
+        });
+    })
+    .then(function(role) {
+      console.log(role);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+
+};
+
+/**
+ * modelNames is an string
+ * roleName is a string
+ */
+PermissionService.prototype.addPermission = function(roleName, modelName, type, relation) {
+  relation = relation && relation.toLowerCase() === 'owner' ? 'owner' : 'role';
+
+  Promise.bind({}, Role.findOne({
+      name: roleName
+    })
+    .then(function(role) {
+      this.role = role;
+      return Model.findOne({
+        name: modelName
+      });
+    })
+    .then(function(model) {
+      return Permission.create({
+        model: model.id,
+        action: type,
+        role: this.role.id,
+        createdBy: 1,
+        relation: relation
+      });
+    })
+    .then(function(newPermission) {
+      sails.log('new ' + type + ' permission', newPermission);
+    })
+    .catch(sails.log.error));
+
+};
 
 module.exports = new PermissionService();
